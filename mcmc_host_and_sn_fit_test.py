6c45b84065afc2769b880fa6f6c93080cbdb1aed
Bhavin Joshi <bajoshi@asu.edu>
2020-09-18 14:37:33 -0400

    Added MCMC testing code for fitting extracted spectra

---
+++ mcmc_host_and_sn_fit_test.py
@@ -0,0 +1,387 @@
+import numpy as np
+from astropy.io import fits
+import emcee
+import corner
+import scipy
+from astropy.cosmology import Planck15
+
+import os
+import sys
+from functools import reduce
+import time
+import datetime
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.gridspec as gridspec
+
+home = os.getenv('HOME')
+stacking_utils = home + '/Documents/GitHub/stacking-analysis-pears/util_codes/'
+
+roman_slitless_dir = home + "/Documents/GitHub/roman-slitless/"
+ext_spectra_dir = home + "/Documents/roman_slitless_sims_results/"
+template_dir = home + "/Documents/roman_slitless_sims_seds/"
+
+sys.path.append(stacking_utils)
+import proper_and_lum_dist as cosmo
+
+def main():
+
+    ext_root = "romansim1"
+    img_suffix = 'Y106_11_1'
+
+    # Read in sed.lst
+    sedlst_header = ['segid', 'sed_path']
+    sedlst = np.genfromtxt(roman_slitless_dir + 'sed_' + img_suffix + '.lst', \
+        dtype=None, names=sedlst_header, encoding='ascii')
+
+    # Read in the extracted spectra
+    ext_spec_filename = ext_spectra_dir + ext_root + '_ext_x1d.fits'
+    ext_hdu = fits.open(ext_spec_filename)
+    print("Read in extracted spectra from:", ext_spec_filename)
+
+    # Set pylinear f_lambda scaling factor
+    pylinear_flam_scale_fac = 1e-17
+
+    # This will come from detection on the direct image
+    # For now this comes from the sedlst generation code
+    # For Y106_11_1
+    host_segids = np.array([475, 755, 548, 207])
+    sn_segids = np.array([481, 753, 547, 241])
+
+    for i in range(len(sedlst)):
+
+        # Get info
+        segid = sedlst['segid'][i]
+
+        # Read in the dummy template passed to pyLINEAR
+        template_name = os.path.basename(sedlst['sed_path'][i])
+
+        if 'salt' not in template_name:
+            continue
+        else:
+            print("Segmentation ID:", segid, "is a SN. Will begin fitting.")
+
+            # Get corresponding host ID
+            hostid = int(host_segids[np.where(sn_segids == segid)[0]])
+            print("I have the following SN and HOST IDs:", segid, hostid)
+
+            # Read in template
+            template = np.genfromtxt(template_dir + template_name, dtype=None, names=True, encoding='ascii')
+
+            # ---------------------------- Set up input params dict ---------------------------- #
+            input_dict = {}
+
+            print("INPUTS:")
+            # ---- SN
+            t = template_name.split('.txt')[0].split('_')
+
+            sn_z = float(t[-1].replace('p', '.').replace('z',''))
+            sn_day = int(t[-2].replace('day',''))
+            print("Supernova input z:", sn_z)
+            print("Supernova day:", sn_day, "\n")
+
+            # ---- HOST
+            h_idx = int(np.where(sedlst['segid'] == hostid)[0])
+            h_path = sedlst['sed_path'][h_idx]
+            th = os.path.basename(h_path)
+
+            th = th.split('.txt')[0].split('_')
+
+            host_z = float(th[-1].replace('p', '.').replace('z',''))
+            host_ms = float(th[-2].replace('p', '.').replace('ms',''))
+
+            host_age_u = th[-3]
+            if host_age_u == 'gyr':
+                host_age = float(th[2])
+            elif host_age_u == 'myr':
+                host_age = float(th[2])/1e3
+
+            print("Host input z:", host_z)
+            print("Host input stellar mass [log(Ms/Msol)]:", host_ms)
+            print("Host input age [Gyr]:", host_age)
+
+            input_dict['host_z'] = host_z
+            input_dict['host_ms'] = host_ms
+            input_dict['host_age'] = host_age
+
+            input_dict['sn_z'] = sn_z
+            input_dict['sn_day'] = sn_day
+
+            # ---------------------------- FITTING ---------------------------- #
+            # ---------- Get spectrum for host and sn
+            host_wav = ext_hdu[hostid].data['wavelength']
+            host_flam = ext_hdu[hostid].data['flam'] * pylinear_flam_scale_fac
+        
+            sn_wav = ext_hdu[segid].data['wavelength']
+            sn_flam = ext_hdu[segid].data['flam'] * pylinear_flam_scale_fac
+
+            # ---- Fit template to HOST and SN
+            noise_level = 0.01  # relative to signal
+            # First assign noise to each point
+            host_flam_noisy, host_ferr = add_noise(host_flam, noise_level)
+            sn_flam_noisy, sn_ferr = add_noise(sn_flam, noise_level)
+
+            # ----------------------- Using emcee ----------------------- #
+            print("\nRunning emcee...")
+
+            # Set jump sizes # ONLY FOR INITIAL POSITION SETUP
+            jump_size_z = 0.01  
+            jump_size_age = 0.1  # in gyr
+            jump_size_tau = 0.1  # in gyr
+            jump_size_av = 0.2  # magnitudes
+            jump_size_lsf = 5.0  # angstroms
+
+            # Labels for corner and trace plots
+            label_list = [r'$z$', r'$Age [Gyr]$', r'$\tau [Gyr]$', r'$A_V [mag]$', r'$LSF [\AA]$']
+
+            # Setup dims and walkers
+            ndim, nwalkers = 5, 100  # setting up emcee params--number of params and number of walkers
+
+            # generating "intial" ball of walkers about best fit from min chi2
+            pos_host = np.zeros(shape=(nwalkers, ndim))
+
+            # Define initial position
+            # The parameter vector is (redshift, age, tau, av, lsf_sigma)
+            # age in gyr and tau in gyr
+            # dust parameter is av not tauv
+            # lsf_sigma in angstroms
+            r_host = np.array([0.1, 1.0, 1.0, 1.0, 10.0])
+            #r_sn = np.array([])
+
+            for i in range(nwalkers):
+
+                rn0 = float(r_host[0] + jump_size_z * np.random.normal(size=1))
+                rn1 = float(r_host[1] + jump_size_age * np.random.normal(size=1))
+                rn2 = float(r_host[2] + jump_size_tau * np.random.normal(size=1))
+                rn3 = float(r_host[3] + jump_size_av * np.random.normal(size=1))
+                rn4 = float(r_host[4] + jump_size_lsf * np.random.normal(size=1))
+
+                rn = np.array([rn0, rn1, rn2, rn3, rn4])
+
+                pos_host[i] = rn
+
+            from multiprocessing import Pool
+
+            with Pool() as pool:
+                
+                sampler = emcee.EnsembleSampler(nwalkers, ndim, logpost, args=[host_wav, host_flam_noisy, host_ferr_noisy], pool=pool)
+                sampler.run_mcmc(pos_host, 1000, progress=True)
+
+            chains = sampler.chain
+            print("Finished running emcee.")
+
+            # plot trace
+            fig1 = plt.figure()
+            ax1 = fig1.add_subplot(111)
+
+            for i in range(nwalkers):
+                for j in range(ndim):
+                    ax1.plot(chains[i,:,j], label=label_list[j], alpha=0.2)
+
+            # Discard burn-in. You do not want to consider the burn in the corner plots/estimation.
+            burn_in = 100
+            samples = sampler.chain[:, burn_in:, :].reshape((-1, ndim))
+
+            # plot corner plot
+            corner.corner(samples, plot_contours='True', labels=label_list, label_kwargs={"fontsize": 14}, show_titles='True', title_kwargs={"fontsize": 14})
+            plt.show()
+
+    return None
+
+
+def get_template(age, tau, tauv, metallicity, \
+    log_age_arr, metal_arr, tau_gyr_arr, tauv_arr, \
+    model_lam_grid_withlines_mmap, model_comp_spec_withlines_mmap):
+
+    """
+    print("\nFinding closest model to --")
+    print("Age [Gyr]:", 10**age / 1e9)
+    print("Tau [Gyr]:", tau)
+    print("Tau_v:", tauv)
+    print("Metallicity [abs. frac.]:", metallicity)
+    """
+
+    # First find closest values and then indices corresponding to them
+    # It has to be done this way because you typically wont find an exact match
+    closest_age_idx = np.argmin(abs(log_age_arr - age))
+    closest_tau_idx = np.argmin(abs(tau_gyr_arr - tau))
+    closest_tauv_idx = np.argmin(abs(tauv_arr - tauv))
+
+    # Now get indices
+    age_idx = np.where(log_age_arr == log_age_arr[closest_age_idx])[0]
+    tau_idx = np.where(tau_gyr_arr == tau_gyr_arr[closest_tau_idx])[0]
+    tauv_idx = np.where(tauv_arr   ==    tauv_arr[closest_tauv_idx])[0]
+    metal_idx = np.where(metal_arr == metallicity)[0]
+
+    model_idx = int(reduce(np.intersect1d, (age_idx, tau_idx, tauv_idx, metal_idx)))
+
+    model_llam = model_comp_spec_withlines_mmap[model_idx]
+
+    chosen_age = 10**log_age_arr[model_idx] / 1e9
+    chosen_tau = tau_gyr_arr[model_idx]
+    chosen_av = 1.086 * tauv_arr[model_idx]
+    chosen_metallicity = metal_arr[model_idx]
+
+    """
+    print("\nChosen model index:", model_idx)
+    print("Chosen model parameters -- ")
+    print("Age [Gyr]:", chosen_age)
+    print("Tau [Gyr]:", chosen_tau)
+    print("A_v:", chosen_av)
+    print("Metallicity [abs. frac.]:", chosen_metallicity)
+    """
+
+    return model_llam
+
+def apply_redshift(restframe_wav, restframe_lum, redshift):
+
+    dl = cosmo.luminosity_distance(redshift)  # returns dl in Mpc
+    dl = dl * 3.09e24  # convert to cm
+
+    redshifted_wav = restframe_wav * (1 + redshift)
+    redshifted_flux = restframe_lum / (4 * np.pi * dl * dl * (1 + redshift))
+
+    return redshifted_wav, redshifted_flux
+
+def loglike(theta, x, data, err):
+    
+    z, age, tau, av, lsf_sigma = theta
+
+    y = model(x, z, age, tau, av, lsf_sigma)
+    #print("Model func result:", y)
+
+    # ------- Vertical scaling factor
+    alpha = np.sum(data * y / err**2) / np.sum(y**2 / err**2)
+    #print("Alpha:", "{:.2e}".format(alpha))
+
+    y = y * alpha
+
+    lnLike = -0.5 * np.sum((y-data)**2/err**2)
+    
+    return lnLike
+
+def logprior(theta):
+
+    z, age, tau, av, lsf_sigma = theta
+    
+    # Make sure model is not older than the Universe
+    # Allowing at least 100 Myr for the first galaxies to form after Big Bang
+    age_at_z = Planck15.age(z).value  # in Gyr
+    age_lim = age_at_z - 0.1  # in Gyr
+
+    if ( 0.01 <= z <= 6.0  and  0.01 <= age <= age_lim  and  0.01 <= tau <= 100.0  and  0.0 <= av <= 3.0  and  10.0 <= lsf_sigma <= 300.0  ):
+        return 0.0
+    
+    return -np.inf
+
+def logpost(theta, x, data, err):
+
+    lp = logprior(theta)
+    
+    if not np.isfinite(lp):
+        return -np.inf
+    
+    lnL = loglike(theta, x, data, err)
+    
+    return lp + lnL
+
+def model(x, z, age_gyr, tau_gyr, av, lsf_sigma):
+    """
+    This function will return the closest BC03 template 
+    from a large grid of pre-generated templates.
+
+    Expects to get the following arguments
+    x: observed wavelength grid
+    z: redshift to apply to template
+    age: age of SED in Gyr
+    tau: exponential SFH timescale in Gyr
+    av: visual dust extinction
+    lsf_sigma: in angstroms
+    """
+
+    current_age = np.log10(age_gyr * 1e9)  # because the saved age parameter is the log(age[yr])
+    current_tau = tau_gyr  # because the saved tau is in Gyr
+    tauv = av / 1.086
+    current_tauv = tauv
+    current_metallicity = 0.02  # Force it to only choose from the solar metallicity CSP models
+
+    model_llam = get_template(current_age, current_tau, current_tauv, current_metallicity, \
+        log_age_arr, metal_arr, tau_gyr_arr, tauv_arr, model_lam_grid, model_grid)
+
+    # ------ Apply redshift
+    model_lam_z, model_flam_z = apply_redshift(model_lam_grid, model_llam, z)
+
+    # ------ Apply LSF
+    model_lsfconv = scipy.ndimage.gaussian_filter1d(input=model_flam_z, sigma=lsf_sigma)
+
+    # ------ Downgrade to grism resolution
+    model_mod = np.zeros(len(x))
+
+    ### Zeroth element
+    lam_step = x[1] - x[0]
+    idx = np.where((model_lam_z >= x[0] - lam_step) & (model_lam_z < x[0] + lam_step))[0]
+    model_mod[0] = np.mean(model_lsfconv[idx])
+
+    ### all elements in between
+    for j in range(1, len(x) - 1):
+        idx = np.where((model_lam_z >= x[j-1]) & (model_lam_z < x[j+1]))[0]
+        model_mod[j] = np.mean(model_lsfconv[idx])
+    
+    ### Last element
+    lam_step = x[-1] - x[-2]
+    idx = np.where((model_lam_z >= x[-1] - lam_step) & (model_lam_z < x[-1] + lam_step))[0]
+    model_mod[-1] = np.mean(model_lsfconv[idx])
+
+    return model_mod
+
+def add_noise(sig_arr, noise_level):
+    """
+    This function will vary the flux randomly within 
+    the noise level specified. It assumes the statistical
+    noise is Gaussian.
+    The final error array returned is Poissonian noise.
+    """
+    # Poisson noise: does the signal have to be in 
+    # units of photons or electrons for sqrt(N) to 
+    # work? like I cannot use sqrt(signal) in physical 
+    # units and call it Poisson noise?
+
+    sigma_arr = noise_level * sig_arr
+
+    spec_noise = np.zeros(len(sig_arr))
+    err_arr = np.zeros(len(sig_arr))
+
+    for k in range(len(sig_arr)):
+
+        mu = sig_arr[k]
+        sigma = sigma_arr[k]
+
+        # Now vary flux using numpy random.normal
+        # mu and the resulting new flux value HAVE TO BE POSITIVE!
+        if mu >= 0:
+            spec_noise[k] = np.random.normal(mu, sigma, 1)
+        elif mu < 0:
+            spec_noise[k] = np.nan
+
+        if spec_noise[k] < 0:
+            max_iters = 10
+            iter_count = 0
+            while iter_count < max_iters:
+                spec_noise[k] = np.random.normal(mu, sigma, 1)
+                iter_count += 1
+                if spec_noise[k] > 0:
+                    break
+            # if it still hasn't given a positive number after max_iters
+            # then revert it back to whatever the signal was before randomly varying
+            if (iter_count >= max_iters) and (spec_noise[k] < 0):
+                spec_noise[k] = sig_arr[k]
+
+        # err_arr[k] = np.sqrt(spec_noise[k])
+        err_arr[k] = noise_level * spec_noise[k]
+
+    return spec_noise, err_arr
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
