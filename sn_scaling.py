be8b450511ddf5f2a6a4fb146a89b86849df90c2
Bhavin Joshi <bajoshi@asu.edu>
2020-09-25 07:52:33 -0400

    Started code to find template scaling

---
+++ sn_scaling.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def main():
+
+    # Read in SALT2 SN IA file from Lou
+    salt2_spec = np.genfromtxt(roman_sims_seds + "salt2_template_0.txt", \
+        dtype=None, names=['day', 'lam', 'flam'], encoding='ascii')
+
+    # Read in g-band filter curve
+    # This is 
+
+	return None
+
+if __name__ == '__main__':
+	main()
+	sys.exit(0)
+
