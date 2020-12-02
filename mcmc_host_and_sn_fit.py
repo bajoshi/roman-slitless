cb16a949b3022fc752fd9794ed71e466718b5f13
Bhavin Joshi <bajoshi@asu.edu>
2020-12-02 10:53:03 -0500

    Testing fitting again with tuned starting positions

--- mcmc_host_and_sn_fit.py
+++ mcmc_host_and_sn_fit.py
@@ -172,6 +172,7 @@ def loglike_host(theta, x, data, err):
     plt.show()
     sys.exit(0)
     """
+    
 
     return lnLike
 
@@ -874,7 +875,7 @@ def main():
     host_segids = np.array([475, 755, 548, 207])
     sn_segids = np.array([481, 753, 547, 241])
 
-    for i in range(len(sedlst)):
+    for i in range(400, len(sedlst)):
 
         # Get info
         segid = sedlst['segid'][i]
@@ -1371,7 +1372,7 @@ def main():
             #rhost_init = get_optimal_fit(args_host, object_type='host')
             #sys.exit(0)
 
-            rhost_init = np.array([0.92, 13.0, 3.0, 0.1, 0.0])
+            rhost_init = np.array([0.44, 12.9, 5.0, 0.2, 3.0])
             print(f"{bcolors.GREEN}Starting position for HOST from where ball of walkers will be generated:\n", rhost_init, f"{bcolors.ENDC}")
             print("logpost at starting position for HOST galaxy:", logpost_host(rhost_init, host_wav, host_flam, host_ferr))
 
@@ -1436,6 +1437,8 @@ def main():
 
                 run_emcee('host', nwalkers, ndim_host, logpost_host, pos_host, args_host, hostid)
                 read_pickle_make_plots('host', ndim_host, args_host, truth_arr_host, label_list_host, hostid, img_suffix)
+
+        sys.exit(0)
 
     return None
 
