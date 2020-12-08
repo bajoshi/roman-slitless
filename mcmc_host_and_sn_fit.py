b846b78eec767e10c341f2c585ae1ffeb207280b
Bhavin Joshi <bajoshi@asu.edu>
2020-12-08 08:14:59 -0500

    New starting pos for fitting. Rerunning to get more accurate stellar masses

--- mcmc_host_and_sn_fit.py
+++ mcmc_host_and_sn_fit.py
@@ -165,7 +165,7 @@ def loglike_host(theta, x, data, err):
     #lnLike = -0.5 * (1 + stretch_fac) * chi2
 
     #print("Pure chi2 term:", np.nansum( (y-data)**2/err**2 ))
-    #print("Second error term:", np.nansum(np.log(2 * np.pi * err**2)))
+    #print("Second error term:", np.nansum( np.log(2 * np.pi * err**2) ))
     #print("log likelihood HOST:", lnLike)
 
     """
@@ -881,7 +881,7 @@ def main():
     print("Read in sed.lst from:", sedlst_path)
 
     # Read in the extracted spectra
-    ext_spec_filename = ext_spectra_dir + 'plffsn2_run_nov30/' + ext_root + '_ext_x1d.fits'
+    ext_spec_filename = ext_spectra_dir + 'plffsn2_run_dec7/' + ext_root + '_ext_x1d.fits'
     ext_hdu = fits.open(ext_spec_filename)
     print("Read in extracted spectra from:", ext_spec_filename)
 
@@ -894,7 +894,7 @@ def main():
     host_segids = np.array([475, 755, 548, 207])
     sn_segids = np.array([481, 753, 547, 241])
 
-    for i in range(500, len(sedlst)):
+    for i in range(len(sedlst)):
 
         # Get info
         segid = sedlst['segid'][i]
@@ -1392,13 +1392,13 @@ def main():
             #sys.exit(0)
 
             if hostid == 207:
-                rhost_init = np.array([1.96, 15.3,  1.0, 1.0, 0.0])
+                rhost_init = np.array([1.96, 13.2,  1.0, 1.0, 0.0])
             elif hostid == 475:
-                rhost_init = np.array([0.44, 12.85, 2.0, 0.5, 3.5])
+                rhost_init = np.array([0.44, 10.7, 2.0, 0.5, 3.5])
             elif hostid == 548:
-            	rhost_init = np.array([1.59, 14.5, 3.5, 2.0, 0.0])
+            	rhost_init = np.array([1.59, 12.3, 3.5, 2.0, 0.0])
             elif hostid == 755:
-            	rhost_init = np.array([0.92, 13.5, 1.0, 1.0, 0.0])
+            	rhost_init = np.array([0.92, 11.3, 1.0, 1.0, 0.0])
 
             print(f"{bcolors.GREEN}Starting position for HOST from where ball of walkers will be generated:\n", rhost_init, f"{bcolors.ENDC}")
             print("logpost at starting position for HOST galaxy:", logpost_host(rhost_init, host_wav, host_flam, host_ferr))
