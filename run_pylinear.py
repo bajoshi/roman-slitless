Fixed order of operations in scaling pylinear sim image with exptime

run_pylinear.py
@@ -83,16 +83,19 @@ for oldf in glob.glob('*_flt.fits'):
         sci = hdul[('SCI',1)].data    # the science image
         size = sci.shape              # dimensionality of the image
 
+        # update the science extension with random noise
+        sci = sci + np.random.normal(loc=0., scale=sig, size=size)
+
+        # Handling of pixels with negative signal
+        neg_idx = np.where(sci < 0.0)
+        sci[neg_idx] = 0.0  # This is wrong but should allow the rest of the program to work for now
+
         # Multiply the science image with the exptime
         # sci image originally in electrons/s
         sci = sci * exptime  # this is now in electrons
 
-        # update the science extension with random noise
-        hdul[('SCI',1)].data = sci + np.random.normal(loc=0., scale=sig, size=size)
-
-        # Handling of pixels with negative signal
-        neg_idx = np.where(hdul[('SCI',1)].data < 0.0)
-        hdul[('SCI',1)].data[neg_idx] = 0.0  # This is wrong but should allow the rest of the program to work for now
+        # Assign updated sci image to the first [SCI] extension
+        hdul[('SCI',1)].data = sci
 
         # update the uncertainty extension with the sigma
         err = np.sqrt(sci) / exptime