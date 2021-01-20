d7d46f455e85dddf91d02edaae64d4ea2c54128d
Bhavin Joshi <bjoshi5@jhu.edu>
2021-01-20 12:07:37 -0500

    small changes from marcc

--- romansim_sbatch.sh
+++ romansim_sbatch.sh
@@ -5,17 +5,25 @@
 #SBATCH --time=00-00:05:00              # Max time for task. Format is DD-HH:MM:SS
 #SBATCH --mail-type=ALL                 # Send a notification when the job starts, stops, or fails
 #SBATCH --mail-user=bjoshi5@jhu.edu     # send-to address
+#SBATCH -o slurm.romansim.%j.out             # STDOUT (%j = JobId)
+#SBATCH -e slurm.romansim.%j.err             # STDERR (%j = JobId)
+
 
 # Typically need the large memory node because pyLINEAR is memory intensive
 # Use --partition=express while testing code
 
 echo "Starting SLURM script"
+echo $SHELL
 
 set -e
 
 module restore
 
 module load python/3.7-anaconda
+conda init bash
+source ~/.bashrc
+module list
+
 conda activate /home-1/bjoshi5@jhu.edu/data/conda_envs/romanslitless
 
 echo "Starting python code from sbatch script..."
