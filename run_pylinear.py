d240615685ecb34ef2d0bd2e0a51738931193367
Bhavin Joshi <bajoshi@asu.edu>
2021-02-15 10:41:07 -0500

    Started code to run simulation grid on marcc

--- run_pylinear.py
+++ run_pylinear.py
@@ -56,136 +56,147 @@ elif 'plffsn2' in socket.gethostname():
     obsstr = '_plffsn2'
 
 # Figure out the correct filenames depending on which machine is being used
-img_suffix = 'Y106_11_1'
+img_suffix_list = ['Y106_11_1', 'Y106_11_2']
+exptime_list = [300, 600, 900, 1800, 3600]
 
-# Define list files and other preliminary stuff
-segfile = direct_img_dir + 'akari_match_' + img_suffix + '_segmap.fits'
+sim_count = 0
 
-obslst = pylinear_lst_dir + 'obs_' + img_suffix + obsstr + '.lst'
-wcslst = pylinear_lst_dir + 'wcs_' + img_suffix + '.lst'
-sedlst = pylinear_lst_dir + 'sed_' + img_suffix + obsstr + '.lst'
-beam = '+1'
-maglim = 99.0
+for img in img_suffix_list:
 
-fltlst = pylinear_lst_dir + 'flt_' + img_suffix + obsstr + '.lst'
+    img_suffix = img_suffix_list[sim_count]
 
-# make sure the files exist
-assert os.path.isfile(segfile)
-assert os.path.isfile(obslst)
-assert os.path.isfile(sedlst)
-assert os.path.isfile(wcslst)
-assert os.path.isfile(fltlst)
+    # Define list files and other preliminary stuff
+    segfile = direct_img_dir + 'akari_match_' + img_suffix + '_segmap.fits'
 
-print("Using the following paths to lst files and segmap:")
-print("Segmentation map:", segfile)
-print("OBS LST:", obslst)
-print("SED LST:", sedlst)
-print("WCS LST:", wcslst)
-print("FLT LST:", fltlst)
+    obslst = pylinear_lst_dir + 'obs_' + img_suffix + obsstr + '.lst'
+    wcslst = pylinear_lst_dir + 'wcs_' + img_suffix + '.lst'
+    sedlst = pylinear_lst_dir + 'sed_' + img_suffix + obsstr + '.lst'
+    beam = '+1'
+    maglim = 99.0
 
-# ---------------------- Get sources
-sources = pylinear.source.SourceCollection(segfile, obslst, detindex=0, maglim=maglim)
+    fltlst = pylinear_lst_dir + 'flt_' + img_suffix + obsstr + '.lst'
 
-"""
-# Set up and tabulate
-grisms = pylinear.grism.GrismCollection(wcslst, observed=False)
-tabulate = pylinear.modules.Tabulate('pdt', ncpu=0) 
-tabnames = tabulate.run(grisms, sources, beam)
+    # make sure the files exist
+    assert os.path.isfile(segfile)
+    assert os.path.isfile(obslst)
+    assert os.path.isfile(sedlst)
+    assert os.path.isfile(wcslst)
+    assert os.path.isfile(fltlst)
 
-# ---------------------- Simulate
-print("Simulating...")
-simulate = pylinear.modules.Simulate(sedlst, gzip=False)
-fltnames = simulate.run(grisms, sources, beam)
-print("Simulation done.")
+    print("Using the following paths to lst files and segmap:")
+    print("Segmentation map:", segfile)
+    print("OBS LST:", obslst)
+    print("SED LST:", sedlst)
+    print("WCS LST:", wcslst)
+    print("FLT LST:", fltlst)
 
-# ---------------------- Add noise
-print("Adding noise...")
-# check Russell's notes in pylinear notebooks
-# also check WFIRST tech report TR1901
-sky = 1.1      # e/s
-npix = 4096 * 4096
-sky /= npix    # e/s/pix
+    # ---------------------- Get sources
+    sources = pylinear.source.SourceCollection(segfile, obslst, detindex=0, maglim=maglim)
 
-dark = 0.015   # e/s/pix
-read = 10.0    # electrons
+    # Set up and tabulate
+    grisms = pylinear.grism.GrismCollection(wcslst, observed=False)
+    tabulate = pylinear.modules.Tabulate('pdt', ncpu=0) 
+    tabnames = tabulate.run(grisms, sources, beam)
 
-exptime = 900  # seconds
+    # ---------------------- Simulate
+    print("Simulating...")
+    simulate = pylinear.modules.Simulate(sedlst, gzip=False, ncpu=0)
+    fltnames = simulate.run(grisms, sources, beam)
+    print("Simulation done.")
 
-for oldf in glob.glob('*_flt.fits'):
-    print("Working on...", oldf)
+    for e in range(len(exptime_list)):
 
-    # let's save the file in case we want to compare
-    savefile = oldf.replace('_flt', '_flt_noiseless')
-    shutil.copyfile(oldf, savefile)
+        # ---------------------- Add noise
+        print("Adding noise...")
+        # check Russell's notes in pylinear notebooks
+        # also check WFIRST tech report TR1901
+        sky = 1.1      # e/s
+        npix = 4096 * 4096
+        sky /= npix    # e/s/pix
 
-    # open the fits file
-    with fits.open(oldf) as hdul:
-        sci = hdul[('SCI',1)].data    # the science image
-        size = sci.shape              # dimensionality of the image
+        dark = 0.015   # e/s/pix
+        read = 10.0    # electrons
 
-        # update the science extension with sky background and dark current
-        signal = (sci + sky + dark)
+        exptime = exptime_list[e]  # seconds
 
-        # Handling of pixels with negative signal
-        #neg_idx = np.where(sci < 0.0)
-        #sci[neg_idx] = 0.0  # This is wrong but should allow the rest of the program to work for now
+        for oldf in glob.glob('*_flt.fits'):
+            print("Working on...", oldf)
+            print("Putting in an exposure time of:", exptime, "seconds.")
 
-        # Multiply the science image with the exptime
-        # sci image originally in electrons/s
-        signal = signal * exptime  # this is now in electrons
+            #if e == 0:
+            #    # let's save the file in case we want to compare
+            #    savefile = oldf.replace('_flt', '_flt_noiseless')
+            #    shutil.copyfile(oldf, savefile)
 
-        # Randomly vary signal about its mean. Assuming Gaussian distribution
-        # first get the uncertainty
-        variance = signal + read**2
-        sigma = np.sqrt(variance)
-        new_sig = np.random.normal(loc=signal, scale=sigma, size=size)
+            # open the fits file
+            with fits.open(oldf) as hdul:
+                sci = hdul[('SCI',1)].data    # the science image
+                size = sci.shape              # dimensionality of the image
 
-        # now divide by the exptime and subtract the sky again 
-        # to get back to e/s. LINEAR expects a background subtracted image
-        final_sig = (new_sig / exptime) - sky
+                # update the science extension with sky background and dark current
+                signal = (sci + sky + dark)
 
-        # Assign updated sci image to the first [SCI] extension
-        hdul[('SCI',1)].data = final_sig
+                # Handling of pixels with negative signal
+                #neg_idx = np.where(sci < 0.0)
+                #sci[neg_idx] = 0.0  # This is wrong but should allow the rest of the program to work for now
 
-        # update the uncertainty extension with the sigma
-        err = np.sqrt(signal) / exptime
+                # Multiply the science image with the exptime
+                # sci image originally in electrons/s
+                signal = signal * exptime  # this is now in electrons
 
-        hdul[('ERR',1)].data = err
+                # Randomly vary signal about its mean. Assuming Gaussian distribution
+                # first get the uncertainty
+                variance = signal + read**2
+                sigma = np.sqrt(variance)
+                new_sig = np.random.normal(loc=signal, scale=sigma, size=size)
 
-        # now write to a new file name
-        hdul.writeto(oldf, overwrite=True)
+                # now divide by the exptime and subtract the sky again 
+                # to get back to e/s. LINEAR expects a background subtracted image
+                final_sig = (new_sig / exptime) - sky
 
-print("Noise addition done. Check simulated images.")
-print("Check statistics with ds9 and continue with extraction.")
-ts = time.time()
-print("Time taken for simulation:", "{:.2f}".format(ts - start), "seconds.")
-"""
+                # Assign updated sci image to the first [SCI] extension
+                hdul[('SCI',1)].data = final_sig
 
-# ---------------------- Extraction
-grisms = pylinear.grism.GrismCollection(fltlst, observed=True)
-#path = home + '/Documents/roman_slitless_sims_results/tables'
-#tabulate = pylinear.modules.Tabulate('pdt', path=path, ncpu=0)
-#tabnames = tabulate.run(grisms, sources, beam)
+                # update the uncertainty extension with the sigma
+                err = np.sqrt(signal) / exptime
 
-extraction_parameters = grisms.get_default_extraction()
+                hdul[('ERR',1)].data = err
 
-print('\nDefault parameters: range = {lamb0}, {lamb1} A, sampling = {dlamb} A'.format(**extraction_parameters))
+                # now write to a new file name
+                newfilename = oldf.replace('_flt', '_flt_' + img_suffix + '_' str(exptime) + 's')
+                hdul.writeto(newfilename, overwrite=True)
 
-# Set extraction params
-sources.update_extraction_parameters(**extraction_parameters)
-method = 'golden'  # single
-root = 'romansim_' + img_suffix
-logdamp = [-7, -1, 0.1]  # logdamp = -np.inf
+        print("Noise addition done. Check simulated images.")
+        ts = time.time()
+        print("Time taken for simulation:", "{:.2f}".format(ts - start), "seconds.")
 
-print("Extracting...")
-pylinear.modules.extract.extract1d(grisms, sources, beam, logdamp, method, root, path, ncpu=16, group=False)
+        # ---------------------- Extraction
+        grisms = pylinear.grism.GrismCollection(fltlst, observed=True)
 
-print("Simulation and extraction done.")
-try:
-    te = time.time() - ts
-    print("Time taken for extraction:", "{:.2f}".format(te), "seconds.")
-except NameError:
-    pass
+        extraction_parameters = grisms.get_default_extraction()
+
+        print('\nDefault parameters: range = {lamb0}, {lamb1} A, sampling = {dlamb} A'.format(**extraction_parameters))
+
+        # Set extraction params
+        sources.update_extraction_parameters(**extraction_parameters)
+        method = 'golden'  # single
+        root = 'romansim_' + img_suffix + '_' str(exptime) + 's'
+        logdamp = [-7, -1, 0.1]  # logdamp = -np.inf
+
+        print("Extracting...")
+        pylinear.modules.extract.extract1d(grisms, sources, beam, logdamp, method, root, path, ncpu=0, group=False)
+
+        print("Simulation and extraction done.")
+        try:
+            te = time.time() - ts
+            print("Time taken for extraction:", "{:.2f}".format(te), "seconds.")
+        except NameError:
+            print("Finished at:", dt.datetime.now())
+
+    # Increment simulation counter
+    # This only increments after all the exptimes 
+    # for a given direct image are simulated
+    sim_count += 1
 
 print("Total time taken:", "{:.2f}".format(time.time() - start), "seconds.")
 
