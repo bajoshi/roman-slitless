cfa0df108c812ca409bbe5f4e204441ee5e896ac
Bhavin Joshi <bajoshi@asu.edu>
2021-03-29 17:07:54 -0400

    Added code to plot and test pylinear extracted spectra

--- mcmc_host_and_sn_fit.py
+++ mcmc_host_and_sn_fit.py
@@ -896,7 +896,7 @@ def get_optimal_fit(args_obj, object_type):
 def get_snr(wav, flux):
 
     spectrum1d_wav = wav * u.AA
-    spectrum1d_flux = flam * u.erg / (u.cm * u.cm * u.s * u.AA)
+    spectrum1d_flux = flux * u.erg / (u.cm * u.cm * u.s * u.AA)
     spec1d = Spectrum1D(spectral_axis=spectrum1d_wav, flux=spectrum1d_flux)
 
     return snr_derived(spec1d)
