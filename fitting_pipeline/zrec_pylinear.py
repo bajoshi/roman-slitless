df5809b790ca351ef3cc9fb06a40c357eeefe664
Bhavin Joshi <bajoshi@asu.edu>
2021-07-16 16:16:01 -0400

    Added code to plot SNR vs recovered accuracy

--- fitting_pipeline/zrec_pylinear.py
+++ fitting_pipeline/zrec_pylinear.py
@@ -146,11 +146,6 @@ fig.clear()
 plt.close(fig)
 
 ###########################################
-# -------------------- z accuracy vs SNR plt
-
-
-
-###########################################
 # -------------------- phase recovery plt
 fig = plt.figure(figsize=(7, 9))
 
@@ -159,7 +154,7 @@ gs = fig.add_gridspec(nrows=11, ncols=1, left=0.05, right=0.95, wspace=0.1)
 ax1 = fig.add_subplot(gs[:8])
 ax2 = fig.add_subplot(gs[8:])
 
-ax2.set_ylabel(r'$\Delta\ \mathrm{Phase}$', fontsize=20)
+ax2.set_ylabel(r'$\Delta \mathrm{Phase}$', fontsize=20)
 ax2.set_xlabel(r'$\mathrm{Phase_{true}}$', fontsize=20)
 
 ax1.set_ylabel(r'$\mathrm{Phase_{inferred}}$', fontsize=20)
@@ -172,7 +167,6 @@ phase900err = np.vstack((cat['phase900_lowerr'], cat['phase900_uperr']))
 phase1800err = np.vstack((cat['phase1800_lowerr'], cat['phase1800_uperr']))
 phase3600err = np.vstack((cat['phase3600_lowerr'], cat['phase3600_uperr']))
 
-"""
 # --- EXPTIME 900 seconds
 ax1.errorbar(cat['phase_true'], phase900, 
     yerr=phase900err, markersize=3.0, fmt='o', color='goldenrod', elinewidth=0.5)
@@ -188,7 +182,6 @@ ax1.errorbar(cat['phase_true'], phase1800,
 ax2.errorbar(cat['phase_true'], phase1800 - cat['phase_true'], 
     yerr=phase1800err, markersize=3.0, fmt='o', color='dodgerblue', elinewidth=0.5)
 ax2.axhline(y=0.0, ls='--', color='gray', lw=2.0)
-"""
 
 # --- EXPTIME 3600 seconds
 ax1.errorbar(cat['phase_true'], phase3600, 
@@ -210,6 +203,9 @@ ax2.set_xlim(-19, 50)
 fig.savefig(results_dir + 'pylinearrecovery_phase.pdf', 
     dpi=200, bbox_inches='tight')
 
+fig.clear()
+plt.close(fig)
+
 ###########################################
 # -------------------- Av recovery plt
 fig = plt.figure(figsize=(7, 9))
@@ -219,7 +215,7 @@ gs = fig.add_gridspec(nrows=11, ncols=1, left=0.05, right=0.95, wspace=0.1)
 ax1 = fig.add_subplot(gs[:8])
 ax2 = fig.add_subplot(gs[8:])
 
-ax2.set_ylabel(r'$\Delta\ \mathrm{A_v}$', fontsize=20)
+ax2.set_ylabel(r'$\Delta \mathrm{A_v}$', fontsize=20)
 ax2.set_xlabel(r'$\mathrm{A_{v;\, true}}$', fontsize=20)
 
 ax1.set_ylabel(r'$\mathrm{A_{v;\, inferred}}$', fontsize=20)
@@ -232,7 +228,6 @@ av900err = np.vstack((cat['Av900_lowerr'], cat['Av900_uperr']))
 av1800err = np.vstack((cat['Av1800_lowerr'], cat['Av1800_uperr']))
 av3600err = np.vstack((cat['Av3600_lowerr'], cat['Av3600_uperr']))
 
-"""
 # --- EXPTIME 900 seconds
 ax1.errorbar(cat['Av_true'], av900, 
     yerr=av900err, markersize=3.0, fmt='o', color='goldenrod', elinewidth=0.5)
@@ -248,7 +243,6 @@ ax1.errorbar(cat['Av_true'], av1800,
 ax2.errorbar(cat['Av_true'], av1800 - cat['Av_true'], 
     yerr=av1800err, markersize=3.0, fmt='o', color='dodgerblue', elinewidth=0.5)
 ax2.axhline(y=0.0, ls='--', color='gray', lw=2.0)
-"""
 
 # --- EXPTIME 3600 seconds
 ax1.errorbar(cat['Av_true'], av3600, 
@@ -271,7 +265,48 @@ ax2.set_xlim(0.0, 5.0)
 fig.savefig(results_dir + 'pylinearrecovery_dust.pdf', 
     dpi=200, bbox_inches='tight')
 
-#plt.show()
+fig.clear()
+plt.close(fig)
+
+###########################################
+# -------------------- Plot SNR vs % accuracy
+fig = plt.figure(figsize=(9, 5))
+
+gs = fig.add_gridspec(nrows=12, ncols=1, left=0.15, right=0.95, wspace=0.1)
+
+ax1 = fig.add_subplot(gs[:4])
+ax2 = fig.add_subplot(gs[4:8])
+ax3 = fig.add_subplot(gs[8:])
+
+# Axis labels
+ax1.set_ylabel(r'$\frac{z_\mathrm{inferred} - z_\mathrm{true}}{1 + z_\mathrm{true}}$', fontsize=15)
+ax2.set_ylabel(r'$\Delta \mathrm{Phase}$', fontsize=15)
+ax3.set_ylabel(r'$\Delta \mathrm{A_v}$', fontsize=15)
+
+ax3.set_xlabel(r'$\mathrm{SNR}$', fontsize=15)
+
+# Plotting
+ax1.scatter(cat['SNR3600'], z3600acc, s=7, color='k',  zorder=2)
+ax1.axhline(y=0.0, ls='--', lw=2.0, color='gray', zorder=1)
+
+ax2.scatter(cat['SNR3600'], phase3600 - cat['phase_true'], s=7, color='k',  zorder=2)
+ax2.axhline(y=0.0, ls='--', lw=2.0, color='gray', zorder=1)
+
+ax3.scatter(cat['SNR3600'], av3600 - cat['Av_true'], s=7, color='k',  zorder=2)
+ax3.axhline(y=0.0, ls='--', lw=2.0, color='gray', zorder=1)
+
+#ax1.set_xlim(10, 150)
+#ax2.set_xlim(10, 150)
+#ax3.set_xlim(10, 150)
+
+ax1.set_ylim(-0.015, 0.015)
+
+fig.savefig(results_dir + 'pylinearrecovery_snr.pdf',
+    dpi=200, bbox_inches='tight')
+
+
+
+
 
 
 
