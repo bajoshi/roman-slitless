4cfb6b9b26babf5a4676ba1d83216ffcc14f4b89
Bhavin Joshi <bajoshi@asu.edu>
2021-08-03 17:22:07 -0400

    Restarting sim

--- fitting_pipeline/fit_sn_romansim.py
+++ fitting_pipeline/fit_sn_romansim.py
@@ -330,8 +330,8 @@ def main():
 
     # ----------------------- Loop over all simulated and extracted SN spectra ----------------------- #
     # Arrays to loop over
-    pointings = np.arange(2, 3)
-    detectors = np.arange(1, 5, 1)
+    pointings = np.arange(0, 1)
+    detectors = np.arange(1, 2, 1)
 
     for pt in pointings:
         for det in detectors:
