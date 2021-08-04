2500adcec14f716f0d07e89f4cf10f9638b0bf0c
Bhavin Joshi <bajoshi@asu.edu>
2021-08-04 09:55:55 -0400

    Updated sim to workaround memory issues

--- run_pylinear.py
+++ run_pylinear.py
@@ -316,7 +316,7 @@ def gen_img_suffixes():
 
     # Arrays to loop over
     pointings = np.arange(0, 1)
-    detectors = np.arange(3, 19, 1)
+    detectors = np.arange(2, 19, 1)
 
     img_filt = 'Y106_'
 
@@ -416,9 +416,9 @@ def main():
 
     # Set some other params
     img_suffix_list = gen_img_suffixes()
-    exptime_list = [3600, 900]
+    exptime_list = [6000, 1500]
     nobs_list = [2, 3, 4]  # no of dithers
-    roll_angle_list = [0.0, 5.0, 10.0, 15.0, 20.0]
+    roll_angle_list = [0.0, 10.0, 20.0]
 
     dir_img_filt = 'hst_wfc3_f105w'
     disp_elem = 'P127'
